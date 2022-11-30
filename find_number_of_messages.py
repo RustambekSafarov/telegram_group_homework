64b65dcb59189599b9f3933e45a389fc770f7880
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-11-30 12:47:57 +0500

    done

--- find_number_of_messages.py
+++ find_number_of_messages.py
@@ -1,6 +1,7 @@
-from read_data import read_data 
+from read_data import read_data
 
-def find_number_of_messages(data: dict)->int:
+
+def find_number_of_messages(data: dict) -> int:
     """
     Get the total number of messages.
 
@@ -8,9 +9,16 @@ def find_number_of_messages(data: dict)->int:
         data (dict): Dictionary containing the data of the json file.
     Returns:
         int: Total number of messages.
-    
+
     """
-    
-    return read_data
+    messages = data['messages']
+    count = 0
+    for m in messages:
+
+        if m['type'] == 'message':
+            count += 1
+    return count
+
+
 data = read_data('data/result.json')
 print(find_number_of_messages(data))
--- read_data.py
+++ read_data.py
@@ -1,19 +1,21 @@
 import json
 
-def read_data(file_path: str)->dict:
+
+def read_data(file_path: str) -> dict:
     """
     This function will read the json file and return the data as a dictionary.
-    
+
     Parameters:
         file_path (str): Path of the file to be read
     Returns:
         dict: Dictionary containing the data of the json file.
-    
+
     """
     data = open(file_path).read()
     data = json.loads(data)
-    
-    #open file
+
+    # open file
     return data
+
+
 file_path = 'data/result.json'
-print(read_data(file_path))
