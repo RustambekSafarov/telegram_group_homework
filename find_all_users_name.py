e71b71e1da8413598a2b3fbbffd7c34988a23960
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-11-30 12:59:19 +0500

    done

--- find_all_users_name.py
+++ find_all_users_name.py
@@ -1,12 +1,24 @@
 from read_data import read_data
 
-def find_all_users_name(data: dict)->list:
+
+def find_all_users_name(data: dict) -> list:
     """
     This function will find all the users in the json file and return the list of users name.
- 
+
     Parameters:
         data (dict): Dictionary containing the data of the json file.
     Returns:
         list: List containing all the users name.
     """
-    return
+    messages = data['messages']
+    users = []
+    for m in messages:
+        if m['type'] == 'message':
+            if users.count(m['from']) == 0:
+                users.append(m['from'])
+
+    return users
+
+
+data = read_data('data/result.json')
+print(find_all_users_name(data))
